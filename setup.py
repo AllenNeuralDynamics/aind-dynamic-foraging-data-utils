d1b7f58eafc190892cbb7df4b6ae1dd3eb22d4fe
rachelstephlee <rachelstephlee@gmail.com>
2024-02-27 14:12:10 -0800

    fix(): Finally fixed pip install -e
    Followed the template to fix it. Coverage covers nearly everything but the src.

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 from setuptools import setup
 
-setup(
-    name="dynamic_foraging_analysis",
-    version="1.0",
-    packages=["dynamic_foraging_analysis"]
-)
-
 if __name__ == "__main__":
     setup()
