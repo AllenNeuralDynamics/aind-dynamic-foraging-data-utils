f3624b66eb579c7479ab25e636950243ab165757
rachelstephlee <rachelstephlee@gmail.com>
2025-02-03 15:29:47 -0800

    addressing some of alex's comments.

--- src/aind_dynamic_foraging_data_utils/code_ocean_utils.py
+++ src/aind_dynamic_foraging_data_utils/code_ocean_utils.py
@@ -8,7 +8,6 @@ Important utility functions for formatting the data
 
     enrich_df_trials_fm
 """
-import itertools
 import json
 import os
 import warnings
@@ -21,10 +20,13 @@ from codeocean.data_asset import DataAssetAttachParams
 
 from aind_dynamic_foraging_data_utils import nwb_utils
 
+URL = "https://api.allenneuraldynamics-test.org/v1/behavior_analysis/mle_fitting"
 
 def generate_data_asset_attach_params(data_asset_IDs, mount_point=None):
     """
     generate_data_asset_attach_params is a helper function for attach_data
+    data_asset_IDs:  list of data asset IDs, i.e. the 16 hash string for the data asset in CO.
+    mount_point: the mount point for the data asset. Default is None.
     """
     data_assets = []
     for ID in data_asset_IDs:
@@ -38,7 +40,12 @@ def generate_data_asset_attach_params(data_asset_IDs, mount_point=None):
 def attach_data(data_asset_IDs, token_name="CUSTOM_KEY"):
     """
     attach_data attaches a list of data_asset_ID to the capsule.
-    Note that the list of data_asset_IDs should be <100"
+    data_asset_IDs: list of data asset IDs, i.e. the 16 hash string for the data asset in CO.
+    token_name: the name of the token in the environment variable. Default is CUSTOM_KEY.
+                see more info here:
+                https://docs.codeocean.com/user-guide/code-ocean-api/authentication#to-create-an-access-token
+
+    Note that the list of data_asset_IDs should be <100 or you may risk CO crashing.
     example: attach_data(da_data['processed_CO_dataID'].to_list())
     """
     if len(data_asset_IDs) > 100:
@@ -120,97 +127,11 @@ def get_all_df_for_nwb(filename_sessions, loc="../scratch/", interested_channels
         df_trials = df_trials.reset_index()
         df_trials.to_csv(loc + "df_trials.csv", index=False)
 
-
-"""
-fix lickspout_y, lickspout_y1, lickspout_y2
-to test:
-try data assets
-
-behavior_746345_2024-11-18_09-46-17_processed_2024-11-19_19-15-06:
-             9b6d248d-ad7b-49dd-b8d7-d4549e87e722
-behavior_746345_2024-12-04_09-16-20_processed_2025-01-04_00-49-53:
-            ff364262-0e04-4cc9-a34a-41083fec1213
-
-
-older data (before 12/2024) have lickspout_y, but not lickspout_y1, lickspout_y2
-newer data (after 12/2024) have lickspout_y1, lickspout_y2.
-def get_all_df_for_nwb(filename_sessions, loc="../scratch/", interested_channels=None):
-    get_all_df_for_nwb gets all the dataframes for the NWB and saves it
-    iteratively onto a location in '../scratch/' or a specified location
-    example: get_all_df_for_nwb(filename_sessions, loc =
-                '../scratch/kenta_dec2/', interested_channels = interested_channels)
-    if not os.path.exists(loc):
-        os.makedirs(loc)
-        print(f"Directory created: {loc}")
-    else:
-        print(f"Saving at: {loc}")
-
-    print(f"Saving channels: {interested_channels}")
-
-    for idx, nwb_file in enumerate(filename_sessions):
-
-        nwb = nwb_utils.load_nwb_from_filename(nwb_file)
-        ses_idx = nwb_file.split("/")[-1].replace("behavior_", "").rsplit("_", 1)[0]
-        print(f"CURRENTLY RUNNING {idx+1}/{len(filename_sessions)}: {ses_idx}")
-        print(
-            "-----------------------------------------------------------------------------------------"
-        )
-
-        # sessions
-        df_session = nwb_utils.create_df_session(nwb)
-        df_session["ses_idx"] = ses_idx
-
-        # trials
-        df_ses_trials = nwb_utils.create_df_trials(nwb)
-        df_ses_trials["ses_idx"] = ses_idx
-        # to take out if all sessions are processed post-1/1/2025
-
-        if 'lickspout_position_y' in df_ses_trials.columns:
-            df_ses_trials.rename(columns = {'lickspout_position_y':'lickspout_position_y1'})
-            df_ses_trials['lickspout_position_y2'] = df_ses_trials['lickspout_position_y1']
-
-        # FIP
-        df_ses_fip = nwb_utils.create_fib_df(nwb, tidy=True)
-        if interested_channels:
-            df_ses_fip = df_ses_fip[df_ses_fip["event"].isin(interested_channels)]
-
-        # events
-        df_ses_events = nwb_utils.create_events_df(nwb)
-        df_ses_events["ses_idx"] = ses_idx
-
-        if idx == 0:
-            df_session.to_csv(loc + "df_sess.csv", index=False)
-            df_ses_fip.to_csv(loc + "df_fip.csv", index=False)
-            df_ses_events.to_csv(loc + "df_events.csv", index=False)
-            df_ses_trials.to_csv(loc + "df_trials.csv", index=False)
-
-            # SAVE column information
-            df_ses_trials_0_columns = df_ses_trials.columns.values
-        else:
-            df_session.to_csv(loc + "df_sess.csv", mode="a", index=False, header=False)
-
-
-            df_ses_fip.to_csv(loc + "df_fip.csv", mode="a", index=False, header=False)
-            df_ses_events.to_csv(
-                loc + "df_events.csv", mode="a", index=False, header=False
-            )
-
-            # TODO: ensure that the df_ses_trials_0_columns is the order to save it in
-            # you'll need to change the order of df_ses_trials
-            # this should be an option in to_csv.
-            df_ses_trials.to_csv(
-                loc + "df_trials.csv", mode="a", index=False, header=False
-            )
-
-"""
-
-
 def check_avail_model_by_nwb_name(nwb_name):
     """
     check_avail_model_by_nwb_name checks all available models fitted to a given session
     nwb_name
     """
-    URL = "https://api.allenneuraldynamics-test.org/v1/behavior_analysis/mle_fitting"
     filter = {
         "nwb_name": nwb_name,  # Session id,
     }
@@ -261,7 +182,6 @@ def get_foraging_model_info(
         df_trials_fm["R_kernel"] = pd.NA
 
     df_sess_params = []
-    URL = "https://api.allenneuraldynamics-test.org/v1/behavior_analysis/mle_fitting"
     for nwb_name in nwb_names:
         filter = {
             "nwb_name": nwb_name,  # Session id,
@@ -333,88 +253,3 @@ def get_foraging_model_info(
 
     else:
         return df_trials_fm, df_sess_fm
-
-
-def enrich_df_trials_fm(df_trials_fm):
-    """
-    enrich_df_trials_fm: enriches the df_trials_fm with additional columns
-    like RPE_earned, RPE_all, Q_chosen, Q_unchosen, Q_sum, Q_Delta, Q_change
-
-    RPE_earned: earned_reward - chosen_values
-    RPE_all: (earned_reward + extra_reward) - chosen_values
-
-    """
-    df_trials_fm_enriched = pd.DataFrame()
-    models = df_trials_fm["model_name"].unique()
-    sessions = np.unique(df_trials_fm["ses_idx"])
-    for i_iter, (ses_idx, model_name) in enumerate(itertools.product(sessions[:], models)):
-        df_ses = df_trials_fm[
-            (df_trials_fm["ses_idx"] == ses_idx) & (df_trials_fm["model_name"] == model_name)
-        ]
-
-        choices = df_ses.choice.map({0: "L", 1: "R", 2: "I"}).values
-        chosen_values = np.nan * np.zeros(len(df_ses))
-        unchosen_values = np.nan * np.zeros(len(df_ses))
-        chosen_kernels = np.nan * np.zeros(len(df_ses))
-        unchosen_kernels = np.nan * np.zeros(len(df_ses))
-        chosen_probabilities = np.nan * np.zeros(len(df_ses))
-        unchosen_probabilities = np.nan * np.zeros(len(df_ses))
-        chosen_stay_probabilities = np.nan * np.zeros(len(df_ses))
-        # chosen_licks = np.nan * np.zeros(len(df_ses))
-        for i_idx in range(len(df_ses)):
-            choice = choices[i_idx]
-            if choice == "I" and "ignore" not in model_name:
-                chosen_values[i_idx] = np.nan
-                chosen_kernels[i_idx] = np.nan
-                chosen_probabilities[i_idx] = np.nan
-                chosen_stay_probabilities[i_idx] = np.nan
-                # chosen_licks[i_idx] = np.nan
-            else:
-                chosen_values[i_idx] = df_ses[choice + "_value"].values[i_idx]
-                chosen_kernels[i_idx] = df_ses[choice + "_kernel"].values[i_idx]
-                chosen_probabilities[i_idx] = df_ses[choice + "_prob"].values[i_idx]
-                if choice != "I":
-                    unchosen_values[i_idx] = df_ses[{"L": "R", "R": "L"}[choice] + "_value"].values[
-                        i_idx
-                    ]
-                    unchosen_probabilities[i_idx] = df_ses[
-                        {"L": "R", "R": "L"}[choice] + "_prob"
-                    ].values[i_idx]
-                    unchosen_kernels[i_idx] = df_ses[
-                        {"L": "R", "R": "L"}[choice] + "_kernel"
-                    ].values[i_idx]
-                    # chosen_licks[i_idx] = df_ses['licks '+choice].values[i_idx]
-                if i_idx < len(df_ses) - 1:
-                    chosen_stay_probabilities[i_idx] = df_ses[choice + "_prob"].values[i_idx + 1]
-        for i_mod, mod in enumerate(models):
-            df_ses.loc[:, ["Q_chosen"]] = chosen_values
-            df_ses.loc[:, ["Q_unchosen"]] = unchosen_values
-            df_ses.loc[:, ["Q_sum"]] = df_ses["L_value"].values + df_ses["R_value"].values
-            df_ses.loc[:, ["Q_Delta"]] = df_ses["Q_chosen"].values - df_ses["Q_unchosen"].values
-            df_ses.loc[:, ["Q_change"]] = np.concatenate([[0], np.diff(chosen_values)])
-
-            df_ses.loc[:, ["P_chosen"]] = chosen_probabilities
-            df_ses.loc[:, ["P_unchosen"]] = unchosen_probabilities
-            df_ses.loc[:, ["P_sum"]] = df_ses["L_prob"].values + df_ses["R_prob"].values
-            df_ses.loc[:, ["P_Delta"]] = df_ses["P_chosen"].values - df_ses["P_unchosen"].values
-            df_ses.loc[:, ["P_change"]] = np.concatenate([[0], np.diff(chosen_probabilities)])
-
-            df_ses.loc[:, ["K_chosen"]] = chosen_kernels
-            df_ses.loc[:, ["K_unchosen"]] = unchosen_kernels
-            df_ses.loc[:, ["K_sum"]] = df_ses["L_kernel"].values + df_ses["R_kernel"].values
-            df_ses.loc[:, ["K_Delta"]] = df_ses["K_chosen"].values - df_ses["K_unchosen"].values
-            df_ses.loc[:, ["K_change"]] = np.concatenate([[0], np.diff(chosen_kernels)])
-
-            df_ses.loc[:, ["Cprobstay"]] = chosen_stay_probabilities
-            # to fix: should reward be earned reward? extra reward
-            # # (i.e. manual reward and auto-rewards)
-            df_ses.loc[:, ["RPE_earned"]] = df_ses["earned_reward"].astype(float) - chosen_values
-            df_ses.loc[:, ["RPE_all"]] = (
-                df_ses["earned_reward"].astype(float) + df_ses["extra_reward"].astype(float)
-            ) - chosen_values
-            df_ses.loc[pd.isna(df_ses["choice"]), "RPE_earned"] = np.nan
-            df_ses.loc[pd.isna(df_ses["choice"]), "RPE_all"] = np.nan
-            # df_ses.loc[:,['licks_chosen']] = chosen_licks
-
-        df_trials_fm_enriched = pd.concat([df_trials_fm_enriched, df_ses], axis=0)
-    return df_trials_fm_enriched
