11dd752ac0031f58a1f69ad7be041f824caca7b7
rachelstephlee <rachelstephlee@gmail.com>
2025-05-29 15:13:09 -0700

    removed a import by accident.

--- src/aind_dynamic_foraging_data_utils/code_ocean_utils.py
+++ src/aind_dynamic_foraging_data_utils/code_ocean_utils.py
@@ -17,6 +17,7 @@ from codeocean import CodeOcean
 from codeocean.data_asset import DataAssetAttachParams
 from aind_data_access_api.document_db import MetadataDbClient
 from aind_dynamic_foraging_data_utils import nwb_utils
+from aind_analysis_arch_result_access.han_pipeline import get_mle_model_fitting
 
 
 def get_subject_assets(subject_id, processed=True):
