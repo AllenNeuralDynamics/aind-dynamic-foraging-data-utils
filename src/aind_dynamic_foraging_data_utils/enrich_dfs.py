83bfe50f4893b012afe2f5381e74812fe7010ec2
rachelstephlee <rachel.lee@alleninstitute.org>
2025-06-13 04:49:18 +0000

    added documentaiton and made all dataframes tidy for the baseline removal

--- src/aind_dynamic_foraging_data_utils/enrich_dfs.py
+++ src/aind_dynamic_foraging_data_utils/enrich_dfs.py
@@ -12,11 +12,12 @@ from scipy.stats import zscore
 import itertools
 
 
-def extract_time_window(x, timepoints, alignment_events, offsets, absolute_time, align_in_trial):
+def extract_time_window(trial_i, timepoints, alignment_events,
+                        offsets, absolute_time, align_in_trial):
     """
-    extract out time window around the offsets around go_cue.
+    extract out time window around the offsets of alignment events
     Args:
-        x (pd.Series): row of trial data from df_trials
+        trial_i (pd.Series): row of trial data from df_trials
         timepoints (list): timestamps for a particular session
         alignment_events (list): 2 timepoints in trial to align the offsets to
         offsets (list): Offsets from the 2 timepoints to extract time window
@@ -26,41 +27,52 @@ def extract_time_window(x, timepoints, alignment_events, offsets, absolute_time,
         window (list): list of timepoints given in_session or in_trial (if align_in_trial)
     """
     # t_start, t_end for the trial
-    t_start = x[alignment_events[0]] + offsets[0] + x[absolute_time]
-    t_end = x[alignment_events[1]] + offsets[1] + x[absolute_time]
+    t_start = trial_i[alignment_events[0]] + offsets[0] + trial_i[absolute_time]
+    t_end = trial_i[alignment_events[1]] + offsets[1] + trial_i[absolute_time]
     # window of timestamps in session time
     window = timepoints[(timepoints > t_start) & (timepoints < t_end)]
-    return window - x[absolute_time] if align_in_trial else window
+    return window - trial_i[absolute_time] if align_in_trial else window
 
 
-def extract_signal_window(x, timepoints, varpoints, alignment_events, offsets, absolute_time):
+def extract_signal_window(trial_i, timepoints, varpoints,
+                          alignment_events, offsets, absolute_time):
     """
-    extract out signal window around the offsets around go_cue.
+    extract out signal for the time window around offsets around alignment events.
     Args:
-        x (pd.Series): row of trial data from df_trials
+        trial_i (pd.Series): row of trial data from df_trials
         timepoints (list): timestamps for a particular session
         varpoints (list): signal for a particular session
         alignment_events (list): 2 timepoints in trial to align the offsets to
         offsets (list): Offsets from the 2 timepoints to extract time window
         absolute_time (str): column name in df_trials that defines start of trial in session
-        align_in_trial (Bool): return the time window in_trial (as opposed to in_session)
     Returns:
         window (list): list of timepoints given in_session or in_trial (if align_in_trial)
     """
     # t_start, t_end for the trial
-    t_start = x[alignment_events[0]] + offsets[0] + x[absolute_time]
-    t_end = x[alignment_events[1]] + offsets[1] + x[absolute_time]
+    t_start = trial_i[alignment_events[0]] + offsets[0] + trial_i[absolute_time]
+    t_end = trial_i[alignment_events[1]] + offsets[1] + trial_i[absolute_time]
     return varpoints[(timepoints > t_start) & (timepoints < t_end)]
 
 
-def get_df_fip_trials(df_fip, df_trials):
+def enrich_fip_in_df_trials(df_fip, df_trials):
     """
-    Processes df_fip and df_trials, computing z-scored data and aligning timestamps.
+    Processes df_fip and df_trials, computing z-scored data and align FIP data in df_trials
     Args:
         df_fip (pd.DataFrame): A DataFrame with FIP data, each row a timepoint for a signal
         df_trials (pd.DataFrame): A DataFrame of trials (each row a trial)
     Returns:
-        Tuple (df_fip, df_trials) with updated trial alignment and z-scored data.
+        df_fip_z (pd.DataFrame): df_fip updated with a data_z column for
+                                z-scored data within a session
+        df_fip_trials (pd.DataFrame):
+                        df_trials updated with FIP data from df_fip
+                        Each trial now has the timestamps and activity for
+                        1 second before goCue to 1 second before
+                        next goCue. new columns added as:
+                        timestamps_in_session_{event}: timestamps in session time for signal 'event'
+                        timestamps_in_trial_{event}: timestamps in trial time for signal 'event'
+                        data_{event}: corresponding data for signal 'event'
+                        data_z_{event}: corresponding z-scored data for signal 'event'
+
     """
 
     # get the goCue of the next trial defined
@@ -72,19 +84,24 @@ def get_df_fip_trials(df_fip, df_trials):
     alignment_events = ['goCue_start_time_in_trial', 'goCue_start_time_next_in_trial']
     offsets = [-1, -1]
 
-    df_fip.loc[:, 'data_z'] = df_fip.groupby(['ses_idx', 'event'])['data'].transform(
+    # updated dataframes
+    df_trials_fip = df_trials.copy()
+    df_fip_z = df_fip.copy()
+
+    # z-score the data
+    df_fip_z.loc[:, 'data_z'] = df_fip_z.groupby(['ses_idx', 'event'])['data'].transform(
                                                  lambda x: zscore(x, ddof=1, nan_policy='omit'))
-    for (ses_idx, event), df_fip_i in df_fip.groupby(['ses_idx', 'event']):
+    for (ses_idx, event), df_fip_i in df_fip_z.groupby(['ses_idx', 'event']):
         # pull fip data into df_trials
-        df_trials_ses = df_trials.loc[df_trials['ses_idx'] == ses_idx, :]
+        df_trials_ses = df_trials_fip.loc[df_trials_fip['ses_idx'] == ses_idx, :]
         if len(df_trials_ses) == 0:
             continue
         timepoints = df_fip_i['timestamps'].values
         absolute_time = 'goCue_start_time_in_session'
-        df_trials.loc[df_trials['ses_idx'] == ses_idx, f'timestamps_in_trial_{event}'] = (
+        df_trials_fip.loc[df_trials_fip['ses_idx'] == ses_idx, f'timestamps_in_trial_{event}'] = (
             df_trials_ses.apply(
-                lambda x: extract_time_window(
-                    x,
+                lambda trial_i: extract_time_window(
+                    trial_i,
                     timepoints,
                     alignment_events,
                     offsets,
@@ -94,10 +111,10 @@ def get_df_fip_trials(df_fip, df_trials):
                 axis=1
             )
         )
-        df_trials.loc[df_trials['ses_idx'] == ses_idx, f'timestamps_in_session_{event}'] = (
+        df_trials_fip.loc[df_trials_fip['ses_idx'] == ses_idx, f'timestamps_in_session_{event}'] = (
             df_trials_ses.apply(
-                lambda x: extract_time_window(
-                    x,
+                lambda trial_i: extract_time_window(
+                    trial_i,
                     timepoints,
                     alignment_events,
                     offsets,
@@ -109,22 +126,21 @@ def get_df_fip_trials(df_fip, df_trials):
         )
         for var_event in ['data', 'data_z']:
             varpoints = df_fip_i[var_event].values
-            df_trials.loc[df_trials['ses_idx'] == ses_idx, f'{var_event}_{event}'] = (
+            df_trials_fip.loc[df_trials_fip['ses_idx'] == ses_idx, f'{var_event}_{event}'] = (
                 df_trials_ses.apply(
-                    lambda x: extract_signal_window(x, timepoints, varpoints,
-                                                    alignment_events, offsets, absolute_time),
+                    lambda trial_i: extract_signal_window(trial_i, timepoints, varpoints,
+                                                          alignment_events, offsets, absolute_time),
                     axis=1
                 )
             )
-    return (df_fip, df_trials)
+    return (df_fip_z, df_trials_fip)
 
 
-def tidy_df_trials(df_fip, df_trials, col_prefix_signal='data'):
+def tidy_df_trials_fip(df_fip, df_trials_fip, col_prefix_signal='data'):
     """
-    Converts df_trials into a fully tidy long-format DataFrame.
+    Converts df_trials_fip's columns of FIP data into a tidy long-format columns in df_fip
     - Extracts event names from `timestamps_{event}` columns.
-    - Keeps only `data_{event}_norm` and `data_z_{event}_norm` columns.
-    - Explodes multiple combinations of timestamps and data columns.
+    - Explodes from FIP columns `{col_prefix_signal}_*{event}_norm`
     - Merges the tidied data with df_fip on `ses_idx` and `timestamps`.
 
     Args:
@@ -132,13 +148,16 @@ def tidy_df_trials(df_fip, df_trials, col_prefix_signal='data'):
         df_trials (pd.DataFrame): A DataFrame of trials (each row a trial)
         col_prefix_signal (str, optional): The prefix of the signal column name. Defaults to 'data'.
 
+
     Returns:
-        df_tidy (pd.DataFrame): A long-format DataFrame with merged `df_fip`.
+        df_tidy (pd.DataFrame): df_fip with the untidy df_trials_fip baseline removed data
+                                added in. Two columns for data_norm (baseline removed signal)
+                                and data_z_norm (z-scored baseline removed signal).
     """
     # Step 1: Identify relevant timestamp and data columns
-    timestamp_cols = [col for col in df_trials.columns
+    timestamp_cols = [col for col in df_trials_fip.columns
                       if col.startswith("timestamps_in_session")]
-    data_cols = [col for col in df_trials.columns
+    data_cols = [col for col in df_trials_fip.columns
                  if col.startswith(col_prefix_signal + "_") and col.endswith("_norm")]
 
     # Step 2: Initialize an empty list to store exploded DataFrames
@@ -151,15 +170,15 @@ def tidy_df_trials(df_fip, df_trials, col_prefix_signal='data'):
         matching_data_cols = [col for col in data_cols if event + '_norm' in col]
 
         # Create df_subset with timestamps and all matching data columns
-        df_subset = df_trials[['ses_idx', timestamp_col] + matching_data_cols].copy()
+        df_subset = df_trials_fip[['ses_idx', timestamp_col] + matching_data_cols].copy()
         df_subset = df_subset.rename(columns={timestamp_col: 'timestamps'})
         df_subset['event'] = event  # Add event column
 
         # Rename data columns dynamically by removing the event part
-        for data_col in matching_data_cols:
-            new_data_col = data_col.replace(f"_{event}_norm", "_norm")
-            new_data_col = data_col.replace(f"_{event}", "")
-            df_subset = df_subset.rename(columns={data_col: new_data_col})
+        # Drop all rows with no signals
+        rename_dict = {data_col: data_col.replace(f"_{event}_norm", "_norm")
+                       for data_col in matching_data_cols}
+        df_subset = df_subset.rename(columns=rename_dict)
         df_subset = df_subset.dropna()
 
         # Explode timestamps and data together
@@ -179,28 +198,28 @@ def tidy_df_trials(df_fip, df_trials, col_prefix_signal='data'):
     df_tidy = df_fip.merge(df_exploded, on=['ses_idx', 'timestamps', 'event'], how='left')
     return df_tidy.dropna().reset_index()
 
-
-def remove_tonic_df_fip(df_fip, df_trials_fip, col_prefix_signal='data',
-                        col_prefix_time='timestamps_in_trial',
-                        offset_from_goCue=[-1, 0], tidy=True):
+# TODO: df_trials_fip has to be cleaned up-- i will need to add a copy of it to output
+# i think previously it was included in case
+def remove_tonic_df_fip(df_fip, df_trials, df_trials_fip, col_prefix_signal='data',
+                        col_prefix_time='timestamps_in_trial'):
     """
     Removes tonic activity by normalizing signal data against baseline.
+    baseline is defined as the average activity 1 second before the goCue.
 
     Args:
         df_fip (pd.DataFrame): A DataFrame with FIP data, each row a timepoint for a signal
+        df_trials (pd.DataFrame): A DataFrame of trials (each row a trial)
         df_trials_fip (pd.DataFrame): A DataFrame of trials with signal data per trial
         col_prefix_signal (str, optional): The prefix of the signal column name. Defaults to 'data'.
         col_prefix_time (str, optional): The prefix of the time column name.
                                             Defaults to 'timestamps_in_trial'.
-        offset_from_goCue (list, optional): The baseline time range for normalization
-                                    Time range is given as offsets from goCue of current trial to
-                                    goCue of next trial. Defaults to [-1, 0].
-        tidy (bool, optional): Whether to return a tidy DataFrame. Defaults to True.
-
     Returns:
-        df_fip_tonic (pd.DataFrame): Updated df_fip with the baseline activity removed as columns
-                                'data_{signal}_norm'. Averaged baseline is recorded as
-                                'data_{signal}_baseline'
+        df_fip_tonic (pd.DataFrame): Updated df_fip with the baseline activity added as columns
+                                'data_{event}_norm'.
+        df_trials (pd.DataFrame): df_trials_fip with averaged baseline added as column
+                                'data_{event}_baseline'.
+        df_trials_fip (pd.DataFrame): df_trials_fip with baseline removed activity added
+                                as column 'data_{sevent}_norm'.
     """
     # Step 1: Find all columns with data signals.
     # Skip any columns that already have baseline removed.
@@ -208,13 +227,20 @@ def remove_tonic_df_fip(df_fip, df_trials_fip, col_prefix_signal='data',
                    and not col.endswith('_baseline')
                    and not col.endswith('_norm')]
 
-    # Step 2: Loop through signal by signal, average the signal,
+    # Step 2: Loop through signal by signal, average the signal for baseline,
     # then remove the average activity per trial.
+
+    # The baseline time range for normalization
+    # Time range is given as offsets from goCue of current trial to
+    # goCue of next trial. Defaults to [-1, 0].
+    # Could be added as input argument in the future.
+    offset_from_goCue = [-1, 0]
     for col_signal in col_signals:
         if col_signal + '_norm' in df_trials_fip.columns:
             continue
         signal_name = col_signal.removeprefix('data_z').removeprefix('data').removesuffix('_norm')
-        df_trials_fip.loc[:, col_signal + '_baseline'] = df_trials_fip.apply(
+        # Calculate the baseline for each trial, add to df_trials and df_trials_fip
+        signal_baseline = df_trials_fip.apply(
             lambda x: (
                 np.nanmean(
                     x[col_signal][
@@ -226,15 +252,17 @@ def remove_tonic_df_fip(df_fip, df_trials_fip, col_prefix_signal='data',
             ),
             axis=1
         )
+        df_trials_fip.loc[:, col_signal + '_baseline'] = signal_baseline
+        df_trials.loc[:, col_signal + '_baseline'] = signal_baseline
+        # Normalize the signal by subtracting the baseline activity from the trial
         df_trials_fip.loc[:, col_signal+'_norm'] = df_trials_fip.apply(
             lambda x: x[col_signal] - x[col_signal+'_baseline']
             # Skip calculation if col_signal is NaN
             if not np.isnan(x[col_signal]).all() else np.nan,
             axis=1
         )
-    if tidy:
-        return tidy_df_trials(df_fip, df_trials_fip, col_prefix_signal)
-    return df_trials_fip
+    df_fip_tonic = tidy_df_trials_fip(df_fip, df_trials_fip, col_prefix_signal)
+    return (df_fip_tonic, df_trials, df_trials_fip)
 
 
 def enrich_df_trials_fm(df_trials_fm):
