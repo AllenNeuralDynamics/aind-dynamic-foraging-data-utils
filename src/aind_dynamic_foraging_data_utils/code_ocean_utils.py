69fe8d154c6683915a5a6d14b273d10d8824843f
rachelstephlee <rachel.lee@alleninstitute.org>
2026-03-30 20:39:21 +0000

    accidental rename of choice

--- src/aind_dynamic_foraging_data_utils/code_ocean_utils.py
+++ src/aind_dynamic_foraging_data_utils/code_ocean_utils.py
@@ -389,8 +389,7 @@ def get_foraging_model_info(
     """
     print(f"Retrieving foraging model {model_name}")
     df_trials_fm = df_trials.copy()
-    df_trials_fm = df_trials_fm.rename(columns={"animal_response": "choice"})
-    df_trials_fm["choice_name"] = df_trials_fm["choice"].map({1: "right", 0: "left"})
+    df_trials_fm["choice_name"] = df_trials_fm["animal_response"].map({1: "right", 0: "left"})
 
     df_trials_fm["model_name"] = model_name
     df_trials_fm["L_prob"] = np.nan
@@ -437,7 +436,7 @@ def get_foraging_model_info(
 
         # pull the information
         mouse_choice_idx = df_trials_fm.index[
-            (df_trials_fm["ses_idx"] == sess_idx) & (df_trials_fm["choice"] < 2)
+            (df_trials_fm["ses_idx"] == sess_idx) & (df_trials_fm["animal_response"] < 2)
         ]
 
         # Add check on mouse_choice_idx and length of choice_prob
