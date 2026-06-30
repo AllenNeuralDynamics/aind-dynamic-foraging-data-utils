5a5357aebfd8957303f0011c80a8e7d53e7e29dd
Alex Piet <alexpiet@gmail.com>
2026-06-30 13:47:56 -0700

    dev

--- src/aind_dynamic_foraging_data_utils/nwb_utils.py
+++ src/aind_dynamic_foraging_data_utils/nwb_utils.py
@@ -355,6 +355,10 @@ def create_df_trials(nwb_filename, adjust_time=True, verbose=True):  # NOQA C901
     # not the times at which the valves were opened
     skip_cols = ["right_valve_open_time", "left_valve_open_time"]
 
+    # Manual fix for delay_start_time
+    if 'delay_start_time' in df:
+        df['delay_start_time'] = pd.to_numeric(df['delay_start_time'], errors='coerce')
+
     # compute times relative to start of trial and start of session
     t0 = nwb.trials[SESSION_ALIGNMENT][0]
     drop_cols = []
