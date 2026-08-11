0da2737451938f53ddec3b0811e8695ceefbb45f
Alex Piet <alexpiet@gmail.com>
2026-08-11 13:42:01 -0700

    docstring

--- src/aind_dynamic_foraging_data_utils/code_ocean_utils.py
+++ src/aind_dynamic_foraging_data_utils/code_ocean_utils.py
@@ -24,7 +24,7 @@ from codeocean.data_asset import DataAssetAttachParams
 from aind_dynamic_foraging_data_utils import nwb_utils
 
 
-def get_assets(metadata_version: Literal["v1", "v2"] = "v1," **kwargs):
+def get_assets(metadata_version: Literal["v1", "v2"] = "v1," ** kwargs):
     """
     Top level function that queries either docdb v1 or v2
     metadata_version (str) either "v1" or "v2"
@@ -47,12 +47,10 @@ def get_assets(metadata_version: Literal["v1", "v2"] = "v1," **kwargs):
     extra_filter (dict), docdb query
     input_projection, what fields to return. If empty, returns everything unless searching
         for all subjects (subjects = [])
+    metadata_version (Literal["v1","v2"]) Which version of the AIND metadata to query
+        if metadata_version == "v2", can additionally accept kwarg:
+            acquisition_version = ["v1","v2"]
 
-    if metadata_version == "v2", can additionally accept kwarg:
-        acquisition_version = ["v1","v2"]
-
-    Example
-    results = get_subject_assets(my_id)
     """
     if metadata_version == "v1":
         warnings.warn(
@@ -88,6 +86,9 @@ def get_subject_assets(subject_id, metadata_version: Literal["v1", "v2"] = "v1",
     extra_filter (dict), docdb query
     input_projection, what fields to return. If empty, returns everything unless searching
         for all subjects (subjects = [])
+    metadata_version (Literal["v1","v2"]) Which version of the AIND metadata to query
+        if metadata_version == "v2", can additionally accept kwarg:
+            acquisition_version = ["v1","v2"]
 
     Example
     results = get_subject_assets(my_id)
